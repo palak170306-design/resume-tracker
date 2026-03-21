0ebf8e270d6a971588bfbf6d61793a843c5a0f0e
palak <palak170306@gmail.com>
2026-03-21 18:34:22 +0530

    Add Puter.js integration and implement auth, file system, and AI features

---
+++ app/routes/auth.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Auth:() => Element =()=>{
+    return (
+        <div>Auth</div>
+    )
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -12,11 +12,12 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return(
-    <main className="bg-[url('/assets/public/images/bg-main.svg')] bg-cover">
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
+  
       
       <section className="main-section">
-        <div className="page-heading">
+        <div className="page-heading py-16">
           <h1>Track Your Applications & Resume Ratings </h1>
           <h2>Review your submissions and check AI-powered feedback.</h2>
         </div>
