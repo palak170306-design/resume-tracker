6db0d80b0b991861b7ec9c8e686bf302685ef70a
palak <palak170306@gmail.com>
2026-03-24 21:52:23 +0530

    Integrate Puter.js for authentication and enhance routing in the application

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,10 @@
 import type { Route } from "./+types/home";
 import Navbar from "../components/navbar";
 import {resumes} from "../constants/index";
-import ResumeCard from "../components/resumeCard"
+import ResumeCard from "../components/resumeCard";
+import {usePuterStore} from"~/lib/puter";
+import {useEffect} from"react";
+import {useLocation,useNavigate} from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,6 +14,14 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+
+  const {auth} = usePuterStore();
+  const navigate = useNavigate();
+
+    useEffect( ()=>{
+        if(!auth.isAuthenticated) navigate('/auth?next=/');
+        },
+       [auth.isAuthenticated] )
   return(
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
